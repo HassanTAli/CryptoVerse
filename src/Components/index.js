9fa51b9b52135ac4e4e8faca999b02b11176679b
Hassan Tarek <hassantarekha@gmail.com>
2022-02-14 20:52:44 +0200

    finish Layout

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar/Navbar'
+export { default as Cryptocurrencies } from './Cryptocurrencies/Cryptocurrencies'
+export { default as CryptoDetail } from './CryptoDetail/CryptoDetail'
+export { default as Exchanges } from './Exchanges/Exchanges'
+export { default as HomePage } from './HomePage/HomePage'
+export { default as News } from './News/News'
