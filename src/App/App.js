d47be19623f30bbb0d459c76ec3fbb537ee0a0d8
Hassan Tarek <hassantarekha@gmail.com>
2022-02-15 19:28:57 +0200

    fix copyright place

--- src/App/App.js
+++ src/App/App.js
@@ -26,7 +26,7 @@ const App = () => {
                 <div className='footer'>
                     <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }} >
                         Cryptoverse <br />
-                        All rights reserved @ Hassan T Ali 2022 &copy;
+                        All rights reserved &copy; Hassan T Ali 2022
                     </Typography.Title>
                     <Space>
                         <Link to="/">Home</Link>
